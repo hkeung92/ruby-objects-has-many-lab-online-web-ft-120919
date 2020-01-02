800c604fba102511ce33c5a028f2c53451aaa300
Ho Yin Keung <hykeung92@gmail.com>
2020-01-02 00:11:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,10 +13,11 @@ class Song
   end
   
   def artist_name
-    if @artist == nil
-      return nil
-    else
-      @artist.name
-    end
+    @artist.name
+    # if @artist == nil
+    #   return nil
+    # else
+    #   @artist.name
+    # end
   end
 end
